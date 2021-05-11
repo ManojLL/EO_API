f214c60dc5685e71be270175a1ff445066c0c3af
ManojLL <manojlakshan421@gmail.com>
2021-05-11 23:22:15 +0530

    add project

---
+++ eo/src/test/java/com/eo/eo/EoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eo.eo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
