237af4190c8b2f329f2c68fa73953e0d0860f3bb
ManojLL <manojlakshan421@gmail.com>
2021-05-16 06:45:57 +0530

    update project dependencies

--- src/main/java/com/eo/eo/EoApplication.java
+++ src/main/java/com/eo/eo/EoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EoApplication.class, args);
+    }
 
 }
